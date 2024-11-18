9fbbcbff4218d11f1f939f64cb0b256842a4be31
JosephChege4 <chegejrmungai@gmail.com>
2024-11-18 14:50:55 -0500

    old take

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -8,7 +8,7 @@ from app import app
 
 
 @pytest.fixture
-def setup_client():
+def app_client():
     """Create a test client for the Flask app."""
     with app.test_client() as client:
         yield client
